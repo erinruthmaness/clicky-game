4062be1e3618f9cf4b2652d1e2ac1b6d282b250a
Erin Ruth Maness <erinruthmaness@yahoo.com>
2019-09-03 22:34:04 -0400

    Updates

---
+++ precache-manifest.9662f43e9bf8f746787059b8c5433d24.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490aafa0b7880878e896e1d9f9de6ed2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4d3ab6ab83593b3c5f6d",
+    "url": "/clicky-game/static/css/main.c558eaf5.chunk.css"
+  },
+  {
+    "revision": "119fafb7a81a9dc1f925",
+    "url": "/clicky-game/static/js/2.39751294.chunk.js"
+  },
+  {
+    "revision": "4d3ab6ab83593b3c5f6d",
+    "url": "/clicky-game/static/js/main.81650d66.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  },
+  {
+    "revision": "eb20c9c605354b31411645a617932ae7",
+    "url": "/clicky-game/static/media/$abba1$.eb20c9c6.jpg"
+  },
+  {
+    "revision": "3fb75500135bfc58fefffbbbc22bc505",
+    "url": "/clicky-game/static/media/$abba10$.3fb75500.jpg"
+  },
+  {
+    "revision": "3e98e9ae0ce58da1e7bc25c242906f4b",
+    "url": "/clicky-game/static/media/$abba11$.3e98e9ae.jpg"
+  },
+  {
+    "revision": "f1c9f961ed7e06d76421fcc6220d5820",
+    "url": "/clicky-game/static/media/$abba12$.f1c9f961.jpg"
+  },
+  {
+    "revision": "2286d3a6c771c7f731a7038af110050f",
+    "url": "/clicky-game/static/media/$abba13$.2286d3a6.jpg"
+  },
+  {
+    "revision": "4b53734806990ca045d0a6d66af58a8d",
+    "url": "/clicky-game/static/media/$abba14$.4b537348.jpg"
+  },
+  {
+    "revision": "be29f0d1343888ed5914cb6f2bd805b5",
+    "url": "/clicky-game/static/media/$abba15$.be29f0d1.jpg"
+  },
+  {
+    "revision": "878d1fe2f71e29b9a38b6c0ce13e552c",
+    "url": "/clicky-game/static/media/$abba16$.878d1fe2.jpg"
+  },
+  {
+    "revision": "193003fbb1962fd4b0035c1b4bd72435",
+    "url": "/clicky-game/static/media/$abba2$.193003fb.jpg"
+  },
+  {
+    "revision": "957a12028f87a031bbb8ea5f6d020f4a",
+    "url": "/clicky-game/static/media/$abba3$.957a1202.jpg"
+  },
+  {
+    "revision": "2f3d6ce9561817f8186b6e9ab734a551",
+    "url": "/clicky-game/static/media/$abba4$.2f3d6ce9.jpg"
+  },
+  {
+    "revision": "77f19d5ecd5d571452630eac21152527",
+    "url": "/clicky-game/static/media/$abba5$.77f19d5e.jpg"
+  },
+  {
+    "revision": "d434efcf126af2821598ad42cead1d2c",
+    "url": "/clicky-game/static/media/$abba6$.d434efcf.jpg"
+  },
+  {
+    "revision": "7a76119bd28ef36db3a64cb744f5d712",
+    "url": "/clicky-game/static/media/$abba7$.7a76119b.jpg"
+  },
+  {
+    "revision": "d90f921b4fbeec66577cf400e06b42c0",
+    "url": "/clicky-game/static/media/$abba8$.d90f921b.jpg"
+  },
+  {
+    "revision": "0ab0268ee3094a39cf5d489486a82564",
+    "url": "/clicky-game/static/media/$abba9$.0ab0268e.jpg"
+  },
+  {
+    "revision": "04026feb5534c1cb841ee86c52d93c76",
+    "url": "/clicky-game/static/media/papyrus-dark.04026feb.png"
+  }
+]);
